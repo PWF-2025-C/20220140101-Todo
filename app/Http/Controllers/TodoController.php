94a5223cf8c3a19dd9fe028edcc893a5eff6d2f4
kyy08 <kyyelzz1@gmail.com>
2025-04-25 22:54:06 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,17 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Todo;
-use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         // $todos = Todo::all();
-        $todos = Todo::where('user_id', Auth::id())->get();
-        dd($todos);
-       return view('todo.index'); 
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_done','asc')
+            ->orderBy('created_at','desc')
+            ->get();
+        // dd($todos);
+        return view('todo.index', compact('todos'));
     }
 
+    public function create()
+    {
+        return view('todo.create');
+    }
+
+    public function edit()
+    {
+        return view('todo.edit');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $todo = todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => Auth::id(),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'todo created successfully.');
+    }
+
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return redirect()->route('todo.index')->with('success', 'todo deleted successfully.');
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function index(){
-        return view('user.index');
+    public function index()
+    {
+        // $users = User::where('id', '!=', 1)->orderBy('name')->paginate(10);
+        // return view('user.index', compact('users'));
+
+        $search = request('search');
+
+        if ($search) {
+            $users = User::where(function ($query) use ($search) {
+                $query->where('name', 'like', '%' . $search . '%') // Perbaikan di sini
+                      ->orWhere('email', 'like', '%' . $search . '%'); // Perbaikan di sini
+            })
+            ->orderBy('name')
+            ->where('id', '!=', 1)
+            ->paginate(20)
+            ->withQueryString();
+        } else {
+            $users = User::where('id', '!=', 1)
+            ->orderBy('name')
+            ->paginate(10);
+        }
+
+        return view('user.index', compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete(); // Menghapus user
+        return redirect()->route('user.index')->with('success', 'User  deleted successfully!');
+    }
+
+    public function edit(User $user)
+    {
+        return view('user.edit', compact('user')); // Mengirimkan user ke view edit
     }
 }
